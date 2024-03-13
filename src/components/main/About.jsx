5bc966a56c3dfc2846330beb742560ddc2c99e7a
Dhruv_Seth <dhruvseth090704@gmail.com>
2024-03-13 07:05:08 +0530

    remove extra items

--- src/components/main/About.jsx
+++ src/components/main/About.jsx
@@ -1,5 +1,5 @@
 import { Box, Typography, Grid,styled} from "@mui/material";
-import ProfileImage from './picofme.png';
+import ProfileImage from './dhruvSeth.png';
 
 const ChangeTypo = styled(Typography)`
     background-image:radial-gradient(circle at 33% 100%, #fed373 4%, #f15245 30%, #d92e7f 62%, #9b36b7 85%, #515ecf);
